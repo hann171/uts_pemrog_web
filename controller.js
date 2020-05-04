95d0a92357916e9f181feb4cad85dea031fa869f
Hengky Wijaya <hannw171@outlook.com>
2020-05-05 00:06:05 +0700

    Change controller.js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("REST API Start Working!")
+    response.ok("REST API Start Working!",res)
 };
