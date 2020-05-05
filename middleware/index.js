23774841f8399e98c07f19be64ce174b6dd66708
Hengky Wijaya <hannw171@outlook.com>
2020-05-05 21:53:58 +0700

    Memperbaiki bug verifikasi

--- middleware/index.js
+++ middleware/index.js
@@ -11,28 +11,26 @@ router.post('/user/login', auth.login);
 //alamat halaman otorisasi
 
 //Pelanggan
-router.get('/user/pelanggan/view/servis', verifikasi.verifikasiPelanggan(), con.tampilGroupServis);
-router.get('/user/pelanggan/input/servis', verifikasi.verifikasiPelanggan(), con.tambahServis);
+router.get('/user/pelanggan/view/servis', verifikasi.verifPelanggan(), con.tampilGroupServis);
+router.post('/user/pelanggan/input/servis', verifikasi.verifPelanggan(), con.tambahServis);
 
 //Admin
-router.get('/user/admin/input/montir', verifikasi.verifikasiAdmin(), con.tambahMontir);
-router.get('/user/admin/input/user', verifikasi.verifikasiAdmin(), con.tambahUser);
-router.get('/user/admin/input/sparepart', verifikasi.verifikasiAdmin(), con.tambahSparepart);
-router.get('/user/admin/input/servis', verifikasi.verifikasiAdmin(), con.tambahServis);
-router.get('/user/admin/input/level', verifikasi.verifikasiAdmin(), con.tambahLevel);
+router.post('/user/admin/input/montir', verifikasi.verifikasiAdmin(), con.tambahMontir);
+router.post('/user/admin/input/user', verifikasi.verifikasiAdmin(), con.tambahUser);
+router.post('/user/admin/input/sparepart', verifikasi.verifikasiAdmin(), con.tambahSparepart);
+router.post('/user/admin/input/servis', verifikasi.verifikasiAdmin(), con.tambahServis);
+router.post('/user/admin/input/level', verifikasi.verifikasiAdmin(), con.tambahLevel);
 
-router.get('/user/admin/ubah/montir', verifikasi.verifikasiAdmin(), con.ubahMontir);
-router.get('/user/admin/ubah/user', verifikasi.verifikasiAdmin(), con.ubahUser);
-router.get('/user/admin/ubah/sparepart', verifikasi.verifikasiAdmin(), con.ubahSparepart);
-router.get('/user/admin/ubah/servis', verifikasi.verifikasiAdmin(), con.ubahServis);
-router.get('/user/admin/ubah/level', verifikasi.verifikasiAdmin(), con.ubahLevel);
+router.put('/user/admin/ubah/montir', verifikasi.verifikasiAdmin(), con.ubahMontir);
+router.put('/user/admin/ubah/user', verifikasi.verifikasiAdmin(), con.ubahUser);
+router.put('/user/admin/ubah/sparepart', verifikasi.verifikasiAdmin(), con.ubahSparepart);
+router.put('/user/admin/ubah/servis', verifikasi.verifikasiAdmin(), con.ubahServis);
+router.put('/user/admin/ubah/level', verifikasi.verifikasiAdmin(), con.ubahLevel);
 
-router.get('/user/admin/hapus/montir', verifikasi.verifikasiAdmin(), con.hapusMontir);
-router.get('/user/admin/hapus/user', verifikasi.verifikasiAdmin(), con.hapusUser);
-router.get('/user/admin/hapus/sparepart', verifikasi.verifikasiAdmin(), con.hapusSparepart);
-router.get('/user/admin/hapus/servis', verifikasi.verifikasiAdmin(), con.hapusServis);
-router.get('/user/admin/hapus/level', verifikasi.verifikasiAdmin(), con.hapusLevel);
-
-
+router.delete('/user/admin/hapus/montir', verifikasi.verifikasiAdmin(), con.hapusMontir);
+router.delete('/user/admin/hapus/user', verifikasi.verifikasiAdmin(), con.hapusUser);
+router.delete('/user/admin/hapus/sparepart', verifikasi.verifikasiAdmin(), con.hapusSparepart);
+router.delete('/user/admin/hapus/servis', verifikasi.verifikasiAdmin(), con.hapusServis);
+router.delete('/user/admin/hapus/level', verifikasi.verifikasiAdmin(), con.hapusLevel);
 
 module.exports = router;
--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -1,52 +1,54 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 
-exports.verifikasiPelanggan = function (req, rest, next) {
-    var level = req.body.level;
-    //cek authorization header
-    var tokenWithBearer = req.headers.authorization;
-    if (tokenWithBearer) {
-        var token = tokenWithBearer.split(' ')[1];
-        //verifikasi
-        jwt.verify(token, config.secret, function (err, decoded) {
-            if (err) {
-                return rest.status(401).send({ auth: false, message: 'token tidak terdaftar!' });
-            } else {
-                if (level == 2) {
-                    req.auth = decoded;
-                    next();
-                } else {
-                    return rest.status(401).send({ auth: false, message: 'Gagal mengotorisasi role anda!' });
+exports.verifikasiPelanggan = function verifikasiPelanggan(){
+    return function(req, res, next){
+        var level = req.body.level;
+        //cek authorization header
+        var tokenWithBearer = req.headers.authorization;
+        if(tokenWithBearer){
+            var token = tokenWithBearer.split(' ')[1];
+            //verifikasi
+            jwt.verify(token,config.secret, function(err,decoded){
+                if(err){
+                    return res.status(401).send({auth:false, message:'token tidak terdaftar!'});
+                }else{
+                    if(level==2){
+                        req.auth = decoded;
+                        next();
+                    }else{
+                        return res.status(401).send({auth:false, message:'Gagal mengotorisasi role anda!'});
+                    }
                 }
-            }
-        });
-    } else {
-        return rest.status(401).send({ auth: false, message: 'token tidak tersedia!' });
+            });
+        }else{
+            return rest.status(401).send({auth:false, message:'token tidak tersedia!'});
+        }
     }
 }
 
-exports.verifikasiAdmin = function (req, rest, next) {
-    var level = req.body.level;
-    //cek authorization header
-    var tokenWithBearer = req.headers.authorization;
-    if (tokenWithBearer) {
-        var token = tokenWithBearer.split(' ')[1];
-        //verifikasi
-        jwt.verify(token, config.secret, function (err, decoded) {
-            if (err) {
-                return rest.status(401).send({ auth: false, message: 'token tidak terdaftar!' });
-            } else {
-                if (level == 1) {
-                    req.auth = decoded;
-                    next();
-                } else {
-                    return rest.status(401).send({ auth: false, message: 'Gagal mengotorisasi role anda!' });
+exports.verifikasiAdmin = function verifikasiAdmin(){
+    return function(req, res, next){
+        var level = req.body.level;
+        //cek authorization header
+        var tokenWithBearer = req.headers.authorization;
+        if(tokenWithBearer){
+            var token = tokenWithBearer.split(' ')[1];
+            //verifikasi
+            jwt.verify(token,config.secret, function(err,decoded){
+                if(err){
+                    return res.status(401).send({auth:false, message:'token tidak terdaftar!'});
+                }else{
+                    if(level==1){
+                        req.auth = decoded;
+                        next();
+                    }else{
+                        return res.status(401).send({auth:false, message:'Gagal mengotorisasi role anda!'});
+                    }
                 }
-            }
-        });
-    } else {
-        return rest.status(401).send({ auth: false, message: 'token tidak tersedia!' });
+            });
+        }else{
+            return res.status(401).send({auth:false, message:'token tidak tersedia!'});
+        }
     }
 }
-
-module.exports = verifikasi;
